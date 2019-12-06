26c56147e0da3b0f72702ce1713e823d65953786
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-06 04:22:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
   original = str.split("")
   new_array = []
-  original.each do |str|
+  original.each do|str|
     new_array << original.reverse
   end
   return new_array.join("")
